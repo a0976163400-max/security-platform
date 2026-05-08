49ff1717f17e9a993c1b849a552f7ec64c136ad0
a0976163400-max <a0976163400@gmail.com>
2026-05-09 00:18:59 +0800

    fix stripe route

--- ai_scam_web/app.py
+++ ai_scam_web/app.py
@@ -691,6 +691,7 @@ def create_checkout(plan):
     )
 
     return redirect(session.url)
+
 if __name__ == "__main__":
     port=int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
